973c85b9ebb966bd1b2b1ad9e9bb5d87fd1ea884
suhao <638781@qq.com>
2019-04-22 12:59:53 +0800

    cmake: multi-files

---
+++ camke/demo2/main.cc
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "power.h"
+int main(int argc, char* argv[]) {
+    double result = power(3,5);
+    printf("result is %g\n", result);
+    return 0;
+}
---
+++ camke/demo2/power.cc
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double power(double base, int exponent)
+{
+    int result = base;
+    if (exponent == 0)
+        return 1;
+    for (int i = 0; i < exponent; ++i)
+    {
+        result = result * base;
+    }
+    return result;
+}
